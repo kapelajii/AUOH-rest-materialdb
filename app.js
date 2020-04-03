a54d8187745159e511c7c6dce2a7926563bb3ec1
kapelajii <you@example.com>
2020-04-03 20:59:39 +0300

    added database and implements get, post and delete

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express');
 
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+
+
+const body_parser = require('body-parser');
+
+const material_controller = require('./material_controller');
+
 const port = process.env.PORT || 8080;
 
 const app = express();
-
-const body_parser = require('body-parser');
 
 // req.body.name
 app.use(body_parser.json()); 
@@ -13,8 +21,46 @@ app.use(body_parser.urlencoded({extended:true}));
 
 app.use( (req, res, next) =>{
     console.log(req.method, ' ', req.path);
+    next();
     // GET /api/materials
 
 });
 
-app.listen(port);
+// ---------------------------
+// Restful API
+//---------------------------
+
+const material_model = require('./material_model');
+
+// Create
+// add new material to database
+app.post("/api/material", material_controller.api_post_material);
+    console.log("api_post_material");
+
+// READ
+app.get("/api/materials", material_controller.api_get_materials);
+
+// UPDATE
+
+// DELETE
+app.delete("/api/material/:id", material_controller.api_delete_material);
+
+// ---------------------------
+// Database connection
+//---------------------------
+
+const database_uri = "mongodb+srv://admin:"+process.env.DB_PASS+"@cluster0-gbsqa.mongodb.net/materialdb?retryWrites=true&w=majority"
+
+mongoose.connect(database_uri, {
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false  
+}).then(() =>{
+
+    app.listen(port);
+    console.log("database connected");
+}).catch(err =>{
+    console.log(err);
+});
+
--- material_controller.js
+++ material_controller.js
@@ -1,17 +1,66 @@
-
+const material_model = require('./material_model');
 
 // CREATE
+const api_post_material = (req, res, next) => {
 
-// READ 
-const api_get_materials = (req, res, next) =>{
-    res.sen(JSON.stringify([]));
+    let data ={
+        name: req.body.name,
+        min_density: req.body.min_density,
+        max_density: req.body.max_density,
+        min_strength: req.body.min_strength,
+        max_strength: req.body.max_strength,
 
+        min_strength_density: req.body.min_strength / req.body.max_density,
+        max_strength_density: req.body.max_strength / req.body.min_density
+    };
+
+    let new_material = material_model(data);
+
+    new_material.save().then(()=> {
+        console.log(new_material);
+        res.send(JSON.stringify(new_material));
+    }).catch(err =>{
+        res.status(500);
+        console.log(err);
+    })
 };
 
+// READ
+const api_get_materials = (req, res, next) => {
+    // get all objects from database
+    material_model.find({})
+    // get only data not methods
+    .lean()
+    .then(materials => {
+        res.send(JSON.stringify(materials));
+
+    }).catch(err =>{
+        res.status(500);
+        console.log(err);
+    });    
+};
 
 // UPDATE
 
 // DELETE
+// api/material/5e8773c1a97d8a5e90c94cd8
+
+const api_delete_material = (req, res, next) => {
+
+    let id = req.params.id;
+    let name = req.params.name;
+    
+    material_model.findByIdAndRemove(id).then(()=> {
+        res.send();
+
+    }).catch(err =>{
+        res.status(500);
+        res.send(err.errmsg);
+        console.log(err);
+    });
+};
 
 // Exports
 module.exports.api_get_materials = api_get_materials;
+module.exports.api_post_material = api_post_material;
+module.exports.api_delete_material = api_delete_material;
