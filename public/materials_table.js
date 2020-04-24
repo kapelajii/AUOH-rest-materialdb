87c0c7f38d4d07c9441ba9bdc296a00e30bfeb8b
kapelajii <you@example.com>
2020-04-24 16:38:12 +0300

    Update data to website table

--- public/materials_table.js
+++ public/materials_table.js
@@ -1,7 +1,65 @@
 
-// JQuery
+//jQuery(document)
 
-// wait unitl document is ready
+// "_id": "5e87764c63e79a85ecf73e49",
+// "name": "Steel_3",
+// "min_density": 685849,
+// "max_density": 1,
+// "min_strength": 333,
+// "max_strength": 4,
+// "min_strength_density": 333,
+// "max_strength_density": 0.00000583218755148728,
+
 $(document).ready(() => {
-    let table = $("#materials_table").dataTable({});
+
+    let table = $("#materials_table").DataTable({
+        ajax: {
+            type: "GET",
+            datatype: "json",
+            url: "/api/materials",
+            dataSrc: ""
+        },
+        rowId: "_id",
+        columns: [{
+                data: "_id",
+                type: "readonly",
+                visible: false
+            },
+            {
+                data: "name",
+                type: "text",
+                required: true
+            },
+            {
+                data: "min_density",
+                type: "float",
+                required: true
+            },
+            {
+                data: "max_density",
+                type: "float",
+                required: true
+            },
+            {
+                data: "min_strength",
+                type: "float",
+                required: true
+            },
+            {
+                data: "max_strength",
+                type: "float",
+                required: true
+            },
+            {
+                data: "min_strength_density",
+                type: "float",
+                required: true
+            },
+            {
+                data: "max_strength_density",
+                type: "float",
+                required: true
+            }
+        ]
+    });
 });
